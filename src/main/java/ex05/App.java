fbe10f1b03aa3ae8ae71e01cd524e01f2700a973
Oliver Moreno <olivermoreno@knights.ucf.edu>
2021-06-05 19:03:39 -0400

    Initial solution to problem 5

--- src/main/java/ex05/App.java
+++ src/main/java/ex05/App.java
@@ -4,7 +4,7 @@
  */
 package ex05;
 
-
+import java.util.Scanner;
 /*
 Exercise 5 - Simple Math
 You’ll often write programs that deal with numbers. And depending on the programming language you use, you’ll have to convert the inputs you get to numerical data types.
@@ -18,10 +18,13 @@ What is the second number? 5
 10 - 5 = 5
 10 * 5 = 50
 10 / 5 = 2
+
 Constraints
+
 Values coming from users will be strings. Ensure that you convert these values to numbers before doing the math.
 Keep the inputs and outputs separate from the numerical conversions and other processing.
 Generate a single output statement with line breaks in the appropriate spots.
+
 Challenges
 Revise the program to ensure that inputs are entered as numeric values. Don’t allow the user to proceed if the value entered is not numeric.
 Don’t allow the user to enter a negative number.
@@ -31,6 +34,28 @@ Implement this program as a GUI program that automatically updates the values wh
 
 public class App {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is the first number? ");
+
+        // input for first number
+        String firstNumber = in.nextLine();
+
+        System.out.print("What is the second number? ");
+
+        // input for second number
+        String secondNumber = in.nextLine();
+
+        // converting strings firstNumber and secondNumber to integers
+        int x = Integer.parseInt(firstNumber);
+        int y = Integer.parseInt(secondNumber);
+
+        // string creation
+        String addition = String.format("%d + %d = %d", x, y, x+y);
+        String subtraction = String.format("%d - %d = %d", x, y, x-y);
+        String multiplication = String.format("%d * %d = %d", x, y, x*y);
+        String division = String.format("%d / %d = %d", x, y, x/y);
+
+        // output
+        System.out.printf("%s\n%s\n%s\n%s\n", addition, subtraction, multiplication, division);
     }
 }
